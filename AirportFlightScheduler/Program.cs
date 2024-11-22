1f1b76b2e87941c7f891e92284e23b2590369aeb
Thomas Jones <tjwalkr3@gmail.com>
2024-11-21 18:18:30 -0700

    Cleaned up Program.cs

--- AirportFlightScheduler/Program.cs
+++ AirportFlightScheduler/Program.cs
@@ -9,10 +9,9 @@ public class Program
 {
     static async Task Main(string[] args)
     {
-        AirlineContext context;
         DbContextOptions<AirlineContext> options;
 
-        //Initialize DBContext object
+        //Initialize DbContextOptions object w/ user secret
         try
         {
             var configuration = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
@@ -26,45 +25,12 @@ public class Program
             return;
         }
 
-        // Ask which entry path the user wants to 
-        string DataGenChain = GetValidInput(new List<string> { "1" }, prompt: "Press 1 then [ENTER] to generate data!");
+        Console.WriteLine("Press any key to start data generation...");
+        Console.ReadLine();
 
         Console.WriteLine("Generating data...");
         FlightDataGenerator generator = new(options);
         await generator.GenerateData(15, 3);
         Console.WriteLine("Done.");
-    }
-
-    /// < summary >
-    /// This method allows passing a List of string values that classify as valid inputs, 
-    /// a prompt to instruct users on what inputs to give, and a case sensitivity boolean 
-    /// that determines whether or not user input must match the case of the valid inputs.
-    /// </summary>
-    static string GetValidInput(List<string> validInputs, string prompt, bool isCaseSensitive = false)
-    {
-        string invalidInputMsg = "Sorry, invalid input. Please try again.";
-        Console.WriteLine(prompt);
-        string? userInput = Console.ReadLine();
-
-        if (isCaseSensitive)
-        {
-            while (userInput == null || !validInputs.Contains(userInput))
-            {
-                Console.WriteLine(invalidInputMsg);
-                Console.WriteLine(prompt);
-                userInput = Console.ReadLine();
-            }
-            return userInput;
-        }
-
-        // This code is only run for non-case-sensitive inputs
-        List<string> lowercaseValidInputs = validInputs.ConvertAll(item => item.ToLower());
-        while (userInput == null || !lowercaseValidInputs.Contains(userInput))
-        {
-            Console.WriteLine(invalidInputMsg);
-            Console.WriteLine(prompt);
-            userInput = Console.ReadLine();
-        };
-        return userInput;
     }
 }
