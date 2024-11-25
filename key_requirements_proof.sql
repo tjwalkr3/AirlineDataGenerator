4cfd7b4b1f2b0909b7728c99b06a799a9eaf270c
Thomas Jones <tjwalkr3@gmail.com>
2024-11-24 20:04:21 -0700

    Update key_requirements_proof.sql

--- key_requirements_proof.sql
+++ key_requirements_proof.sql
@@ -1,4 +1,5 @@
--- gets the passenger count and plane capacity for each flight
+-- Key Feature #1
+-- Gets each flight, how many have booked it, the max capacity, and its scheduled arrival and departure timestamps
 with plane_capacities as (
 	select p.id as plane_id, sum(ptst.quantity) as plane_capacity
 	from plane p
@@ -9,22 +10,25 @@ with plane_capacities as (
 	group by p.id
 ),
 passenger_counts as (
-	select sf.id as scheduled_flight_id, sf.plane_id, count(*) as reservation_count
+	select sf.id as scheduled_flight_id, sf.plane_id, sf.departure_time, sf.arrival_time, count(*) as reservation_count
 	from scheduled_flight sf 
 	inner join reservation r
 	on (sf.id = r.scheduled_flight_id)
-	group by sf.id, sf.plane_id
+	group by sf.id, sf.plane_id, sf.departure_time, sf.arrival_time
 )
 select pcts.scheduled_flight_id, 
 	pc.plane_capacity, 
 	pcts.reservation_count, 
-	(pcts.reservation_count / pc.plane_capacity::decimal * 100) as percent_booked
+	(pcts.reservation_count / pc.plane_capacity::decimal * 100) as percent_booked,
+	pcts.departure_time,
+	pcts.arrival_time
 from plane_capacities pc
 inner join passenger_counts pcts
 on (pc.plane_id = pcts.plane_id)
 order by pcts.scheduled_flight_id asc;
 
--- flight performance efficiency function
+-- Key Feature #4
+-- Flight Performance Efficiency function
 -- calculates percentages based on the flights that have been canceled
 create or replace function flight_performance_efficiency() returns table(percent_flights_on_time decimal(10,6), percent_flights_canceled decimal(10,6)) as $$
 	begin
@@ -52,7 +56,8 @@ $$ language plpgsql;
 
 select * from flight_performance_efficiency();
 
--- Flight Estimations Query/Function
+-- Key Feature 5
+-- Flight Revenue Estimation Query/Function
 -- Calculates the expected revenue within a 10 day interval after a given startdate
 create or replace function flight_revenue_estimate(startdate date) 
 returns table(start_date date, end_date date, revenue decimal(10,2)) as $$
@@ -77,5 +82,46 @@ begin
 end;
 $$ language plpgsql;
 
-
 select * from flight_estimate('08-21-24');
+
+-- flight continuity procedure
+-- tracks whether scheduled flights are from airport to airport
+-- logs when a plane "teleports" to another airport without flying
+CREATE OR REPLACE PROCEDURE flight_continuity ()
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    current_row RECORD;
+    -- Variable to hold each row during iteration
+    last_row RECORD;
+    -- Variable to store the last row
+BEGIN
+    FOR current_row IN
+    SELECT
+        id,
+        departure_airport_id,
+        arrival_airport_id,
+        plane_id
+    FROM
+        airline_booking2.scheduled_flight
+    ORDER BY
+        plane_id ASC,
+        departure_time ASC -- Ensure rows are processed in a defined order
+        LOOP
+            -- If last_row is not null, perform the comparison
+            IF last_row IS NOT NULL THEN
+                IF last_row.arrival_airport_id = current_row.departure_airport_id THEN
+                    --RAISE NOTICE 'Row continuity check passed: Plane ID=%, Last Arrival=%, Current Departure=%', last_row.plane_id, last_row.arrival_airport_id, current_row.departure_airport_id;
+                ELSIF last_row.plane_id != current_row.plane_id THEN
+                    --RAISE NOTICE 'Row continuity check passed: new plane: Plane ID=%, Departure=%', last_row.plane_id, current_row.departure_airport_id;
+                ELSE
+                    RAISE WARNING 'Row continuity check failed: Flight ID=%, Last Arrival=%, Current Departure=%', current_row.id, last_row.arrival_airport_id, current_row.departure_airport_id;
+                END IF;
+            END IF;
+            -- Update last_row to hold the current_row for the next iteration
+            last_row := current_row;
+        END LOOP;
+END;
+$$;
+
+CALL flight_continuity();
